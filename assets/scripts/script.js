146745a86de25d2644b23c046f9e7f97ba9bff9a
Kaique Taquette <kaiquetaquette55@gmail.com>
2022-07-27 13:14:54 -0300

    Segundo commit main project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
-const front = "card_front"
-const back = "card_back"
+const FRONT = "card_front"
+const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = ['bootstrap', 'css', 'electron', 'firebase', 'html', 'javascript', 
             'jquery', 'mongo', 'node', 'react'];
@@ -13,7 +15,47 @@ startGame();
 function startGame(){
     cards = creatCardsFromTechs(techs);
     shuffleCards(cards);
-    console.log(cards)
+    initializeCards(cards);
+}
+function initializeCards(cards){
+    let gameBoard = document.getElementById("gameBoard");
+    
+    cards.forEach(card => {
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        creatCardContent(card, cardElement);
+
+        //colocando carta no tabuleiro
+        cardElement.addEventListener('click', flipCard())
+        gameBoard.appendChild(cardElement);
+
+
+
+    })
+
+}
+function creatCardContent(card, cardElement){
+
+    creatCardFace(FRONT, card, cardElement);
+    creatCardFace(BACK, card, cardElement);
+
+}
+function creatCardFace(face, card, element){
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT){
+        let iconElement = document.createElement('img'); 
+        iconElement.classList.add(ICON)
+        iconElement.src = "./assets/imgs/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards){ //shuffle = embaralhar
@@ -33,9 +75,9 @@ creatCardsFromTechs(techs);
 function creatCardsFromTechs(techs){
     let cards = [];
 
-    for(let tech of techs){
+    techs.forEach((tech) =>{
         cards.push(creatPairFromTech(tech));
-    }
+    })
     return cards.flatMap(pair => pair);
 }
 
@@ -55,4 +97,8 @@ function creatIdWithTech(tech){
     return tech + parseInt(Math.random() * 1000);
 }
 
- 
+function flipCard(){
+    
+this.classList.add("flip");
+
+}
